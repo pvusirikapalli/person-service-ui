93170415f978b84b7085ee5471d198120850a7d2
pvusirikapalli <p.vusirikapalli@cgi.com>
2019-12-04 12:06:31 -0700

    Added new date component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DateComponent } from './date/date.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateComponent
   ],
   imports: [
     BrowserModule,
